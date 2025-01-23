842acce7e97d5da0b6cad13fa69b8fc8b92cab9f
sotahagino <sota.hagino@personalplan.jp>
2025-01-23 19:13:43 +0900

    GTMタグの埋め込み

--- apps/frontend-user/src/app/layout.tsx
+++ apps/frontend-user/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import "./globals.css"
 import ClientLayout from './client-layout'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,7 +22,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja" className={inter.className}>
+      <head>
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-54LRK465');
+          `}
+        </Script>
+      </head>
       <body className="font-sans antialiased">
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-54LRK465"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <ClientLayout>
           {children}
         </ClientLayout>
